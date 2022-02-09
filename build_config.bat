c9832683e7c8fc2ac2283d3d1db2b863fe172ed7
Trass3r <trass3r@gmail.com>
2022-02-09 17:15:22 +0100

    try qt 6.2.2

--- build_config.bat
+++ build_config.bat
@@ -10,9 +10,8 @@ REM 5.15.2 msvc2015,msvc2019
 REM 6.2.2 msvc2019
 
 if "%QTVER%"=="" (
-REM    set QTVER=5.12.12
-    set QTVER=5.15.2
-REM    set QTVER=6.2.2
+REM    set QTVER=5.15.2
+    set QTVER=6.2.4
 )
 
 if "%MSVC_VER%"=="" (
